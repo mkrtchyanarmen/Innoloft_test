65fc26b6acb7ad1887bf2fcc9bb8c48d009bfe4d
Armen <armdevjs@gmail.com>
2023-07-30 23:23:06 +0400

    config: eslint and env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
+declare module '*.svg' {
+  const content: React.FC<React.SVGProps<SVGElement>>;
+  export default content;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_URI?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [checker({ typescript: true }), react(), svgr({ exportAsDefault: true })],
+  resolve: {
+    alias: {
+      '@config': resolve(__dirname, './src/config'),
+    },
+  },
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
